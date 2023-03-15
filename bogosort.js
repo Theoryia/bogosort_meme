5777480e1779ef25f83b06f05f9a8828033b0e1c
Theoryia <theokinder@hotmail.co.uk>
2023-03-15 08:44:52 +0000

    final

---
+++ bogosort.js
@@ -0,0 +1,31 @@
+function randomizeString(str) {
+  let randomized = "";
+  while (str.length > 0) {
+    const index = Math.floor(Math.random() * str.length);
+    randomized += str.charAt(index);
+    str = str.slice(0, index) + str.slice(index + 1);
+  }
+  return randomized;
+}
+let count = 0;
+function bogosort(input, target, orgin) {
+  
+  console.log("bogosorting");
+  const startTime = performance.now();
+  while (input !== target) {
+    console.log(`${input} (sorting) count: ${count}`);
+    input = randomizeString(input);
+    count = count + 1;
+    if (count % 1000000 == 0){
+      console.log("sorting " + count)
+    }
+  }
+  const endTime = performance.now();
+  const elapsedTime = endTime - startTime;
+  console.log(`${orgin} -> ${input} (sorted)`);
+  console.log(`Elapsed time: ${elapsedTime.toFixed(4)}ms and Operations: ${count}`);
+}
+
+const target = "hello worlde";
+const input = "worlde hello";
+bogosort(input, target, input);
